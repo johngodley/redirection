b7948e2f7f96c0fd29f710471ab4d43efb60462f
John Godley <johng75@gmail.com>
2013-08-03 10:52:01 +0000

    Code formatting cleanup Use correct Red_Item::get_by_module() to collect RSS items

--- fileio/rss.php
+++ fileio/rss.php
@@ -7,7 +7,7 @@ class Red_Rss_File extends Red_FileIO
 	function collect ($module)
 	{
 		$this->name  = $module->name;
-		$this->items = RE_Log::get_by_module( $module->id );
+		$this->items = Red_Item::get_by_module( $module->id );
 	}
 
 	function feed ()
@@ -22,23 +22,23 @@ class Red_Rss_File extends Red_FileIO
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
 	xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
-	<title><?php echo $title.' - '; bloginfo_rss ('name'); ?></title>
-	<link><?php bloginfo_rss('url') ?></link>
+	<title><?php echo esc_html( $title ).' - '; bloginfo_rss ('name'); ?></title>
+	<link><?php bloginfo_rss( 'url' ) ?></link>
 	<description><?php bloginfo_rss("description") ?></description>
-	<pubDate><?php echo htmlspecialchars (mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false)); ?></pubDate>
-	<generator><?php echo htmlspecialchars ('http://wordpress.org/?v='); bloginfo_rss ('version'); ?></generator>
-	<language><?php echo get_option ('rss_language'); ?></language>
+	<pubDate><?php echo esc_html( mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ) ); ?></pubDate>
+	<generator><?php echo esc_html( 'http://wordpress.org/?v=' ); bloginfo_rss( 'version' ); ?></generator>
+	<language><?php echo get_option( 'rss_language' ); ?></language>
 <?php
 		if (count ($this->items) > 0)
 		{
 			foreach ($this->items as $log) : ?>
 	<item>
-		<title><![CDATA[<?php echo $log->url; ?>]]></title>
-		<link><![CDATA[<?php bloginfo ('home'); echo $log->url; ?>]]></link>
-		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $log->created_at, false); ?></pubDate>
-		<guid isPermaLink="false"><?php print($log->id); ?></guid>
-		<description><![CDATA[<?php echo $log->url; ?>]]></description>
-		<content:encoded><![CDATA[<?php if ($log->referrer) echo 'Referred by '.$log->referrer; ?>]]></content:encoded>
+		<title><?php echo esc_html( $log->url ); ?></title>
+		<link><![CDATA[<?php echo home_url(); echo esc_html( $log->url ); ?>]]></link>
+		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $log->created, false); ?></pubDate>
+		<guid isPermaLink="false"><?php echo $log->id; ?></guid>
+		<description><?php echo esc_html( $log->url ); ?></description>
+		<content:encoded><?php if ( $log->referrer ) echo 'Referred by '.esc_html( $log->referrer ); ?></content:encoded>
 	</item>
 		<?php endforeach; } ?>
 </channel>
@@ -47,5 +47,3 @@ class Red_Rss_File extends Red_FileIO
 		die();
 	}
 }
-
-?>
