2c7fb4757432470ef448ff50f54b9b22247e78d8
John Godley <johng75@gmail.com>
2014-11-21 15:11:03 +0000

    Fix login URL not saving

--- matches/login.php
+++ matches/login.php
@@ -29,12 +29,14 @@ class Login_Match extends Red_Match {
 
 	function show()	{
 		?>
-		</table>
-
-		<p style="padding: 0.5em">
-			<?php _e( 'The target URL will be chosen from one of the following URLs, depending if the user is logged in or out.  Leaving a URL blank means that the user is not redirected.', 'redirection' ); ?>
-		</p>
-		<table class="edit">
+		<tr>
+			<th></th>
+			<td>
+				<p style="padding: 0.5em">
+					<?php _e( 'The target URL will be chosen from one of the following URLs, depending if the user is logged in or out.  Leaving a URL blank means that the user is not redirected.', 'redirection' ); ?>
+				</p>
+			</td>
+		</tr>
 		<tr>
 			<th width="100" valign="top">
 				<?php if ( strlen( $this->url_loggedin ) > 0 ) : ?>
@@ -47,10 +49,10 @@ class Login_Match extends Red_Match {
 				<input style="width: 95%" type="text" name="url_loggedin" value="<?php echo esc_attr( $this->url_loggedin ); ?>"/>
 			</td>
 		</tr>
-		<tr>]
+		<tr>
 			<th width="100" valign="top">
 				<?php if ( strlen( $this->url_loggedout ) > 0 ) : ?>
-					<a target="_blank" href="<?php echo $this->url_loggedout ?>"><?php _e( 'Logged Out', 'redirection' ); ?>:</a>
+					<a target="_blank" href="<?php echo esc_url( $this->url_loggedout ) ?>"><?php _e( 'Logged Out', 'redirection' ); ?>:</a>
 				<?php else : ?>
 					<?php _e( 'Logged Out', 'redirection' ); ?>:
 				<?php endif; ?>
