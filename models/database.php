5074ff1a76aa064bf7469188178afae3f8aed2e5
John Godley <johng75@gmail.com>
2016-12-26 10:41:43 +0000

    Show plugin install error if tables fail to create

--- models/database.php
+++ models/database.php
@@ -125,13 +125,17 @@ class RE_Database {
 	public function install() {
 		global $wpdb;
 
+		$wpdb->show_errors();
+
 		foreach ( $this->get_all_tables() as $sql ) {
 			if ( $wpdb->query( $sql ) === false ) {
+				throw new Exception( 'There was a database error installing Redirection - please post these details to https://github.com/johngodley/redirection/issues' );
 				return false;
 			}
 		}
 
 		$this->createDefaults();
+		$wpdb->hide_errors();
 
 		return true;
 	}
