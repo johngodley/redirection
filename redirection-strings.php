21540c55750ca2bfb38323fe7f91d62d6236a627
John Godley <johng75@gmail.com>
2017-06-05 06:23:09 +0100

    Add generated files for strings and UI

---
+++ redirection-strings.php
@@ -0,0 +1,27 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$redirection_strings = array(
+__( "Failed to load data", "redirection" ), // client/component/options/index.js:31
+__( "Update", "redirection" ), // client/component/options/options-form.js:109
+__( "This will be used to auto-generate a URL if no URL is given. You can use the special tags {{code}}\$dec\${{/code}} or {{code}}\$hex\${{/code}} to have a unique ID inserted (either decimal or hex)", "redirection" ), // client/component/options/options-form.js:100
+__( "Auto-generate URL", "redirection" ), // client/component/options/options-form.js:97
+__( "A unique token allowing feed readers access to Redirection log RSS (leave blank to auto-generate)", "redirection" ), // client/component/options/options-form.js:94
+__( "RSS Token", "redirection" ), // client/component/options/options-form.js:92
+__( "Monitor changes to posts", "redirection" ), // client/component/options/options-form.js:88
+__( "(time to keep logs for)", "redirection" ), // client/component/options/options-form.js:85
+__( "404 Logs", "redirection" ), // client/component/options/options-form.js:84
+__( "(time to keep logs for)", "redirection" ), // client/component/options/options-form.js:81
+__( "Redirect Logs", "redirection" ), // client/component/options/options-form.js:80
+__( "I'm a nice person and I have helped support the author of this plugin", "redirection" ), // client/component/options/options-form.js:76
+__( "Plugin support", "redirection" ), // client/component/options/options-form.js:73
+__( "Failed to save data", "redirection" ), // client/component/options/options-form.js:70
+__( "Your options were updated", "redirection" ), // client/component/options/options-form.js:69
+__( "Don't monitor", "redirection" ), // client/component/options/options-form.js:40
+__( "Forever", "redirection" ), // client/component/options/options-form.js:37
+__( "Two months", "redirection" ), // client/component/options/options-form.js:36
+__( "A month", "redirection" ), // client/component/options/options-form.js:35
+__( "A week", "redirection" ), // client/component/options/options-form.js:34
+__( "A day", "redirection" ), // client/component/options/options-form.js:33
+__( "No logs", "redirection" ), // client/component/options/options-form.js:32
+);
+/* THIS IS THE END OF THE GENERATED FILE */
