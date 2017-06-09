08a6c18382c7931da44c6f382516b2cd12b9cf8c
John Godley <johng75@gmail.com>
2017-06-09 06:59:04 +0100

    Move support page to React

--- client/component/home/index.js
+++ client/component/home/index.js
@@ -9,14 +9,24 @@ import React from 'react';
  */
 
 import Options from 'component/options';
+import Support from 'component/support';
 
 class Home extends React.Component {
+	getContent() {
+		const parts = document.location.search.split( '&' );
+
+		if ( parts[ 1 ] === 'sub=support' ) {
+			return <Support />;
+		}
+
+		return <Options />;
+	}
+
 	render() {
-		return (
-			<div>
-				<Options />
-			</div>
-		);
+		// Will need a better way of doing this once the other pages are in React
+		const content = this.getContent();
+
+		return content;
 	}
 }
 
