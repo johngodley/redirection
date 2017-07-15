ee0a1cf8c5a12fbae8778431cac6b6678a5c7a0f
John Godley <johng75@gmail.com>
2017-07-15 06:00:41 +0100

    Remove old redirect table PHP

--- view/item-list.php
+++ view/item-list.php
@@ -1,32 +1,16 @@
 <?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?>
 <div class="wrap">
-	<h2><?php _e( 'Redirections', 'redirection' ); ?></h2>
+	<h2><?php _e( 'Redirections', 'redirection' ) ?></h2>
 
 	<?php $this->render( 'submenu', array( 'options' => $options ) ); ?>
 
-	<form action="tools.php">
-		<input type="hidden" name="page" value="redirection.php"/>
-
-		<?php $table->search_box( __( 'Search' ), 'search_id' ); ?>
-	</form>
-
-	<form method="POST" action="">
-		<?php $table->display(); ?>
-	</form>
-
-	<div style="clear: both"></div>
+	<div id="react-ui">
+		<noscript>Please enable JavaScript</noscript>
+	</div>
 </div>
 
-<?php $this->render( 'add', array( 'add_to_screen' => true, 'group' => $group, 'hidden' => false ) ); ?>
-
-<script type="text/javascript">
-( function( $ ) {
-	$( document ).ready( function() {
-		var items = new Redirection_Items( $ );
-		var adder = new Redirection_Add( $, '#target', true );
-
-		adder.setup( 'select[name=red_action]', '#add' );
-		items.setup( 'table.items' );
+<script>
+	addLoadEvent( function() {
+		redirection.show( 'react-ui' );
 	} );
-} )( jQuery );
 </script>
