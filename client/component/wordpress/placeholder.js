25c17da8aa0bf5211936d240e27aaff63171dab8
John Godley <johng75@gmail.com>
2017-07-16 08:46:13 +0100

    Add placeholder component

---
+++ client/component/wordpress/placeholder.js
@@ -0,0 +1,15 @@
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+
+const Placeholder = () => {
+	return (
+		<div className="placeholder-container">
+			<div className="placeholder-loading" />
+		</div>
+	);
+};
+
+export default Placeholder;
