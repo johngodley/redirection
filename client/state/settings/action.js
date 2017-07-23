9fa09ff9e19cdafec57c83b2c8e78029bd33b060
John Godley <johng75@gmail.com>
2017-07-23 06:45:32 +0100

    Settings to check for cached values
    If we’ve loaded the settings then no need to load again

--- client/state/settings/action.js
+++ client/state/settings/action.js
@@ -9,47 +9,46 @@ import {
 	SETTING_SAVING,
 	SETTING_SAVED,
 	SETTING_SAVE_FAILED,
+	STATUS_COMPLETE,
 } from './type';
 import getApi from 'lib/api';
 
-export const loadSettings = () => {
-	return dispatch => {
-		getApi( 'red_load_settings' )
-			.then( json => {
-				dispatch( { type: SETTING_LOAD_SUCCESS, values: json.settings, groups: json.groups } );
-			} )
-			.catch( error => {
-				dispatch( { type: SETTING_LOAD_FAILED, error } );
-			} );
+export const loadSettings = () => ( dispatch, getState ) => {
+	if ( getState().settings.loadStatus === STATUS_COMPLETE ) {
+		return null;
+	}
 
-		return dispatch( { type: SETTING_LOAD_START } );
-	};
+	getApi( 'red_load_settings' )
+		.then( json => {
+			dispatch( { type: SETTING_LOAD_SUCCESS, values: json.settings, groups: json.groups } );
+		} )
+		.catch( error => {
+			dispatch( { type: SETTING_LOAD_FAILED, error } );
+		} );
+
+	return dispatch( { type: SETTING_LOAD_START } );
 };
 
-export const saveSettings = settings => {
-	return dispatch => {
-		getApi( 'red_save_settings', settings )
-			.then( json => {
-				dispatch( { type: SETTING_SAVED, values: json.settings, groups: json.groups } );
-			} )
-			.catch( error => {
-				dispatch( { type: SETTING_SAVE_FAILED, error } );
-			} );
+export const saveSettings = settings => dispatch => {
+	getApi( 'red_save_settings', settings )
+		.then( json => {
+			dispatch( { type: SETTING_SAVED, values: json.settings, groups: json.groups } );
+		} )
+		.catch( error => {
+			dispatch( { type: SETTING_SAVE_FAILED, error } );
+		} );
 
-		return dispatch( { type: SETTING_SAVING } );
-	};
+	return dispatch( { type: SETTING_SAVING } );
 };
 
-export const deletePlugin = () => {
-	return dispatch => {
-		getApi( 'red_delete_plugin' )
-			.then( json => {
-				document.location.href = json.location;
-			} )
-			.catch( error => {
-				dispatch( { type: SETTING_SAVE_FAILED, error } );
-			} );
+export const deletePlugin = () => dispatch => {
+	getApi( 'red_delete_plugin' )
+		.then( json => {
+			document.location.href = json.location;
+		} )
+		.catch( error => {
+			dispatch( { type: SETTING_SAVE_FAILED, error } );
+		} );
 
-		return dispatch( { type: SETTING_SAVING } );
-	};
+	return dispatch( { type: SETTING_SAVING } );
 };
