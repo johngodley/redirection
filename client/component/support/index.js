9bf81ce3986a040e0bedc209f49ae4d38849c756
John Godley <johng75@gmail.com>
2017-08-06 10:39:37 +0100

    Clarify the support page
    Add a direct link to the repo and my email

--- client/component/support/index.js
+++ client/component/support/index.js
@@ -1,9 +1,11 @@
+/* global Redirectioni10n */
 /**
  * External dependencies
  */
 
 import React from 'react';
 import { connect } from 'react-redux';
+import { translate as __ } from 'lib/locale';
 
 /**
  * Internal dependencies
@@ -11,7 +13,6 @@ import { connect } from 'react-redux';
 
 import Faq from './faq';
 import Newsletter from './newsletter';
-import Donation from './donation';
 import { loadSettings } from 'state/settings/action';
 
 class Support extends React.Component {
@@ -22,13 +23,27 @@ class Support extends React.Component {
 	}
 
 	render() {
-		const { support = false, newsletter = false } = this.props.values ? this.props.values : {};
+		const { newsletter = false } = this.props.values ? this.props.values : {};
 
 		return (
-			<div style={ { paddingTop: '5px' } }>
-				<Donation support={ support } />
-				<Newsletter newsletter={ newsletter } />
+			<div>
+				<h2>{ __( 'Need help?' ) }</h2>
+				<p>{ __( 'First check the FAQ below. If you continue to have a problem then please disable all other plugins and check if the problem persists.' ) }</p>
+				<p>{ __( 'You can report bugs and new suggestions in the Github repository. Please provide as much information as possible, with screenshots, to help explain your issue.' ) }</p>
+				<p className="github">
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/johngodley/redirection/issues">
+						<img src={ Redirectioni10n.pluginBaseUrl + '/images/GitHub-Mark-64px.png' } width="32" height="32" />
+					</a>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/johngodley/redirection/issues">https://github.com/johngodley/redirection/</a>
+				</p>
+				<p>{ __( "If you want to submit information that you don't want in a public repository then send it directly via {{email}}email{{/email}}.", {
+					components: {
+						email: <a href={ 'mailto:john@urbangiraffe.com?subject=Redirection%20Issue&body=' + encodeURIComponent( 'Redirection: ' + Redirectioni10n.versions ) } />
+					}
+				} ) }
+				</p>
 				<Faq />
+				<Newsletter newsletter={ newsletter } />
 			</div>
 		);
 	}
