0cf1c08443bfe3b166df84448d4ae89ea46b006c
John Godley <johng75@gmail.com>
2017-08-14 07:00:22 +0100

    2.7.1 build JS

--- redirection-version.php
+++ redirection-version.php
@@ -1,4 +1,4 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '2.7' );
-define( 'REDIRECTION_BUILD', '4ed4a07c687f8cb83278c154ffada53f' );
+define( 'REDIRECTION_VERSION', '2.7.1' );
+define( 'REDIRECTION_BUILD', '391a96fde65d52d6155386b23988cf60' );
