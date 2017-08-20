2ee79608b120140e63829c0e9dbdf620b21774ce
John Godley <johng75@gmail.com>
2017-08-20 07:56:47 +0100

    Fix Apache import
    Hadn’t updated the values to match the new Red_Item::create

--- tests/fileio/test-apache.php
+++ tests/fileio/test-apache.php
@@ -14,45 +14,45 @@ class ApacheTest extends WP_UnitTestCase {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'RewriteRule /thing /newthing [301,L]' );
 
-		$this->assertEquals( '/thing', $item['source'] );
-		$this->assertEquals( '/newthing', $item['target'] );
-		$this->assertEquals( '301', $item['code'] );
+		$this->assertEquals( '/thing', $item['url'] );
+		$this->assertEquals( '/newthing', $item['action_data'] );
+		$this->assertEquals( '301', $item['action_code'] );
 	}
 
 	public function testRedirect() {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'Redirect permanent /thing /newthing' );
 
-		$this->assertEquals( '/thing', $item['source'] );
-		$this->assertEquals( '/newthing', $item['target'] );
-		$this->assertEquals( '301', $item['code'] );
+		$this->assertEquals( '/thing', $item['url'] );
+		$this->assertEquals( '/newthing', $item['action_data'] );
+		$this->assertEquals( '301', $item['action_code'] );
 	}
 
 	public function testRedirectQuoted() {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'Redirect permanent "/products/space thing/again thing.html" /products/again-thing/' );
 
-		$this->assertEquals( '/products/space thing/again thing.html', $item['source'] );
-		$this->assertEquals( '/products/again-thing/', $item['target'] );
-		$this->assertEquals( '301', $item['code'] );
+		$this->assertEquals( '/products/space thing/again thing.html', $item['url'] );
+		$this->assertEquals( '/products/again-thing/', $item['action_data'] );
+		$this->assertEquals( '301', $item['action_code'] );
 	}
 
 	public function testRedirect302() {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'Redirect /thing /newthing' );
 
-		$this->assertEquals( '/thing', $item['source'] );
-		$this->assertEquals( '/newthing', $item['target'] );
-		$this->assertEquals( '302', $item['code'] );
+		$this->assertEquals( '/thing', $item['url'] );
+		$this->assertEquals( '/newthing', $item['action_data'] );
+		$this->assertEquals( '302', $item['action_code'] );
 	}
 
 	public function testRedirectMatch() {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'RedirectMatch permanent /thing /newthing' );
 
-		$this->assertEquals( '/thing', $item['source'] );
-		$this->assertEquals( '/newthing', $item['target'] );
-		$this->assertEquals( '301', $item['code'] );
+		$this->assertEquals( '/thing', $item['url'] );
+		$this->assertEquals( '/newthing', $item['action_data'] );
+		$this->assertEquals( '301', $item['action_code'] );
 		$this->assertTrue( $item['regex'] );
 	}
 
@@ -60,9 +60,9 @@ class ApacheTest extends WP_UnitTestCase {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'RedirectMatch /thing /newthing' );
 
-		$this->assertEquals( '/thing', $item['source'] );
-		$this->assertEquals( '/newthing', $item['target'] );
-		$this->assertEquals( '302', $item['code'] );
+		$this->assertEquals( '/thing', $item['url'] );
+		$this->assertEquals( '/newthing', $item['action_data'] );
+		$this->assertEquals( '302', $item['action_code'] );
 		$this->assertTrue( $item['regex'] );
 	}
 
@@ -70,17 +70,17 @@ class ApacheTest extends WP_UnitTestCase {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'Redirect permanent /thing/other /other/thing#1' );
 
-		$this->assertEquals( '/thing/other', $item['source'] );
-		$this->assertEquals( '/other/thing#1', $item['target'] );
-		$this->assertEquals( '301', $item['code'] );
+		$this->assertEquals( '/thing/other', $item['url'] );
+		$this->assertEquals( '/other/thing#1', $item['action_data'] );
+		$this->assertEquals( '301', $item['action_code'] );
 	}
 
 	public function testRewriteNoCode() {
 		$apache = new Red_Apache_File();
 		$item = $apache->get_as_item( 'RewriteRule ^products/reporting /products/ [R,L]' );
 
-		$this->assertEquals( '/products/reporting', $item['source'] );
-		$this->assertEquals( '/products/', $item['target'] );
-		$this->assertEquals( '302', $item['code'] );
+		$this->assertEquals( '/products/reporting', $item['url'] );
+		$this->assertEquals( '/products/', $item['action_data'] );
+		$this->assertEquals( '302', $item['action_code'] );
 	}
 }
