e250e25cb5d556318d283f34416e29a4acaefa6c
John Godley <johng75@gmail.com>
2017-09-09 09:40:45 +0100

    General formatting cleanup

--- modules/wordpress.php
+++ modules/wordpress.php
@@ -63,8 +63,10 @@ class WordPress_Module extends Red_Module {
 
 	public function status_header( $status ) {
 		// Fix for incorrect headers sent when using FastCGI/IIS
-		if ( substr( php_sapi_name(), 0, 3 ) === 'cgi' )
+		if ( substr( php_sapi_name(), 0, 3 ) === 'cgi' ) {
 			return str_replace( 'HTTP/1.1', 'Status:', $status );
+		}
+
 		return $status;
 	}
 
@@ -84,8 +86,7 @@ class WordPress_Module extends Red_Module {
 		if ( $is_IIS ) {
 			header( "Refresh: 0;url=$url" );
 			return $url;
-		}
-		elseif ( $status === 301 && php_sapi_name() === 'cgi-fcgi' ) {
+		} elseif ( $status === 301 && php_sapi_name() === 'cgi-fcgi' ) {
 			$servers_to_check = array( 'lighttpd', 'nginx' );
 
 			foreach ( $servers_to_check as $name ) {
@@ -95,13 +96,13 @@ class WordPress_Module extends Red_Module {
 					exit( 0 );
 				}
 			}
-		}
-		elseif ( $status == 307) {
+		} elseif ( $status == 307 ) {
 			status_header( $status );
 			header( "Cache-Control: no-cache, must-revalidate, max-age=0" );
 			header( "Expires: Sat, 26 Jul 1997 05:00:00 GMT" );
 			return $url;
 		}
+
 		status_header( $status );
 		return $url;
 	}
@@ -118,8 +119,10 @@ class WordPress_Module extends Red_Module {
 
 	public function permalink_redirect_skip( $skip ) {
 		// only want this if we've matched using redirection
-		if ( $this->matched )
+		if ( $this->matched ) {
 			$skip[] = $_SERVER['REQUEST_URI'];
+		}
+
 		return $skip;
 	}
 }
