0816e24a55b1a25adcef485cb161a4d8bee7b6f6
John Godley <johng75@gmail.com>
2017-10-15 10:36:00 +0100

    Bump for version 2.8 beta 2

--- redirection-version.php
+++ redirection-version.php
@@ -1,4 +1,4 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '2.8 beta 1' );
-define( 'REDIRECTION_BUILD', '4322f6ff01d91871335b37fb25fed862' );
+define( 'REDIRECTION_VERSION', '2.8 beta 2' );
+define( 'REDIRECTION_BUILD', '153495d6cde948ef275d75e6648170be' );
