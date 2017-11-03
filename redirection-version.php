102a95cd8354d0522164b0617da0865f085f71fe
John Godley <johng75@gmail.com>
2017-11-03 17:28:29 +0000

    Update JS bundle

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '2.9' );
-define( 'REDIRECTION_BUILD', '23d7f1e9832d8beaad8a71c068fe0194' );
+define( 'REDIRECTION_BUILD', 'b47cc211b1fb0d634d09490a5e886476' );
 define( 'REDIRECTION_MIN_WP', '4.4' );
