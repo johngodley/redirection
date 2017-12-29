8c3ec4c36b41f2995790fc52c7fe088a8580ef88
John Godley <johng75@gmail.com>
2017-12-29 10:03:29 +0000

    Remove ping action
    The REST API sends back a new nonce each request

--- client/state/message/action.js
+++ client/state/message/action.js
@@ -1,4 +1,3 @@
-/* global Redirectioni10n */
 /**
  * Internal dependencies
  */
@@ -7,13 +6,6 @@ import {
 	MESSAGE_CLEAR_ERRORS,
 	MESSAGE_CLEAR_NOTICES,
 } from './type';
-import getApi from 'lib/api';
 
 export const clearErrors = () => ( { type: MESSAGE_CLEAR_ERRORS } );
 export const clearNotices = () => ( { type: MESSAGE_CLEAR_NOTICES } );
-export const ping = () => () => {
-	getApi( 'red_ping' )
-		.then( json => {
-			Redirectioni10n.WP_API_nonce = json.nonce;
-		} );
-};
