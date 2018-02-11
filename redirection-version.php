557b8d43786ad03bef9bc5994c0c446af9db2a1a
John Godley <johng75@gmail.com>
2018-02-11 15:12:53 +0000

    3.2 version bump

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '3.2-beta' );
-define( 'REDIRECTION_BUILD', '4c834f9e175d538274c29e5f67e43692' );
+define( 'REDIRECTION_VERSION', '3.2' );
+define( 'REDIRECTION_BUILD', 'df1f37ef3f6aba1189b89b37737a9a75' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
