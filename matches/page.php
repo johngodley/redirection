1afd0513ea4776e557fa7c9a7aaafe8fe9eb5b19
John Godley <johng75@gmail.com>
2018-10-30 06:44:52 +0000

    Add page match unit test

--- matches/page.php
+++ matches/page.php
@@ -10,7 +10,7 @@ class Page_Match extends Red_Match {
 	}
 
 	public function save( array $details, $no_target_url = false ) {
-		$data = array( 'page' => isset( $details['page'] ) ? $this->sanitize_page( $details['page'] ) : [] );
+		$data = array( 'page' => isset( $details['page'] ) ? $this->sanitize_page( $details['page'] ) : '404' );
 
 		return $this->save_data( $details, $no_target_url, $data );
 	}
