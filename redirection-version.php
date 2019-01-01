186b8b0e1a74ffc91f30575d985667222d358073
John Godley <johng75@gmail.com>
2019-01-01 10:12:17 +0000

    Build 3.7 files

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '3.7' );
-define( 'REDIRECTION_BUILD', 'd33a576694a3157c9266cc170424d313' );
+define( 'REDIRECTION_BUILD', '27a0d2cd06bfd6322873de52297d9b78' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
