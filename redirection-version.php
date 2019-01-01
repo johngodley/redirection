ee35ffeb1981e7a675eaae8ed0869bd9611dc9e7
John Godley <johng75@gmail.com>
2019-01-01 10:15:17 +0000

    Mark as 3.7 beta 1

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '3.7' );
-define( 'REDIRECTION_BUILD', '27a0d2cd06bfd6322873de52297d9b78' );
+define( 'REDIRECTION_VERSION', '3.7 beta 1' );
+define( 'REDIRECTION_BUILD', 'ed59b9aacb9da8830eb374dab1d37bd5' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
