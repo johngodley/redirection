5f25b883f6fc7f511ff2c94ac6c1440ee8cdf0c7
John Godley <johng75@gmail.com>
2019-01-06 17:56:56 +0000

    Build 3.7 beta 2 files

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '3.7 beta 1' );
-define( 'REDIRECTION_BUILD', 'ed59b9aacb9da8830eb374dab1d37bd5' );
+define( 'REDIRECTION_BUILD', '87c4730edc580dee7094b4df8409fa45' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
