587799bc5385d9b7f0ee0c00bf0efd2e434eb027
John Godley <johng75@gmail.com>
2019-01-14 13:48:29 +0000

    Move index detection to function that requires it
    It is possible we could be start at another stage other than the start of the upgrade, and so don’t run the detection code

--- database/schema/240.php
+++ database/schema/240.php
@@ -6,7 +6,6 @@
  * Try and detect this and skip the upgrade
  */
 class Red_Database_240 extends Red_Database_Upgrader {
-	private $existing_index = false;
 	private $existing_column = false;
 
 	public function get_stages() {
@@ -15,20 +14,27 @@ class Red_Database_240 extends Red_Database_Upgrader {
 			'expand_log_ip_column_240' => 'Expand IP size in logs to support IPv6',
 			'convert_int_ip_to_varchar_240' => 'Convert integer IP values to support IPv6',
 			'swap_ip_column_240' => 'Swap IPv4 for IPv6',
-			'add_missing_index_240' => 'Add missing IP index to 404 logs',
 			'convert_title_to_text_240' => 'Expand size of redirect titles',
+			'add_missing_index_240' => 'Add missing IP index to 404 logs',
 		];
+	}
+
+	private function has_weird_ip_index() {
+		global $wpdb;
+
+		$existing = $wpdb->get_row( "SHOW CREATE TABLE `{$wpdb->prefix}redirection_404`", ARRAY_N );
+
+		if ( isset( $existing[1] ) ) {
+			return strpos( strtolower( $existing[1] ), 'key `ip` (`id`)' ) !== false;
+		}
+
+		return false;
 	}
 
 	protected function detect_existing_240( $wpdb ) {
 		$existing = $wpdb->get_row( "SHOW CREATE TABLE `{$wpdb->prefix}redirection_404`", ARRAY_N );
 
 		if ( isset( $existing[1] ) ) {
-			if ( strpos( strtolower( $existing[1] ), 'key `ip` (`id`)' ) !== false ) {
-				// Incorrect index exists for 'id', but called 'ip' - remove so add_missing_index_240 will work
-				$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` drop index ip" );
-			}
-
 			if ( strpos( strtolower( $existing[1] ), '`ip` varchar(45)' ) !== false ) {
 				// IP as varchar exists - don't recreate
 				$this->existing_column = true;
@@ -65,6 +71,10 @@ class Red_Database_240 extends Red_Database_Upgrader {
 	}
 
 	protected function add_missing_index_240( $wpdb ) {
+		if ( $this->has_weird_ip_index() ) {
+			$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` DROP INDEX ip" );
+		}
+
 		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` ADD INDEX `ip` (`ip`)" );
 	}
 
