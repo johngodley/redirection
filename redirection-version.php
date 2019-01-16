dbe4088787bc45ff966feb281f0796449bc1bd51
John Godley <johng75@gmail.com>
2019-01-16 07:53:17 +0000

    3.7.2 release

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '3.7.2-beta-1' );
-define( 'REDIRECTION_BUILD', 'a62d46e5928c3fb2703a9603dd720a95' );
+define( 'REDIRECTION_VERSION', '3.7.2' );
+define( 'REDIRECTION_BUILD', '1db9a83c192c4b52c68ec743e037ef1c' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
