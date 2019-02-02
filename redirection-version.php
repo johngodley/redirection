28f27f7339888d4b9ef02a4126d4e82903c3bcad
John Godley <johng75@gmail.com>
2019-02-02 12:27:28 +0000

    Release 4.0 beta 1

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.0 alpha 2' );
-define( 'REDIRECTION_BUILD', '1d94ee91fda91615a8206e35d6b3a96d' );
+define( 'REDIRECTION_VERSION', '4.0 beta 1' );
+define( 'REDIRECTION_BUILD', '1a4e976a7cd24a20652b3102b6cfa15c' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
