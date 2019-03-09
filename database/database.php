05615ce5e1d36fe8328188a8179ff8f6621960d4
John Godley <johng75@gmail.com>
2019-03-09 13:12:44 +0000

    Fix WP CLI for single site installs

--- database/database.php
+++ database/database.php
@@ -76,7 +76,7 @@ class Red_Database {
 	}
 
 	public static function apply_to_sites( $callback ) {
-		if ( is_network_admin() || defined( 'WP_CLI' ) && WP_CLI ) {
+		if ( is_multisite() && ( is_network_admin() || defined( 'WP_CLI' ) && WP_CLI ) ) {
 			array_map( function( $site ) use ( $callback ) {
 				switch_to_blog( $site->blog_id );
 
