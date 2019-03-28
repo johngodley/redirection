e321c4623074d2f9b2744c3dd6b412f0fec64c2b
John Godley <johng75@gmail.com>
2019-03-28 21:46:15 +0000

    Package 4.2

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.1.1' );
-define( 'REDIRECTION_BUILD', 'fe636e19d78b7c1e78f5ebf2f550f37e' );
+define( 'REDIRECTION_VERSION', '4.2' );
+define( 'REDIRECTION_BUILD', 'f3217f5aec210b6f2ba614068634af6e' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
