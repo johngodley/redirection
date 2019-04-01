77171252d329e4d712cdfacc4f291fa387788b47
John Godley <johng75@gmail.com>
2019-04-01 07:09:22 +0100

    Transparently URL encode URLs
    This will encode the path part of a URL, but not the slashes or other characters

--- models/url-match.php
+++ models/url-match.php
@@ -16,9 +16,31 @@ class Red_Url_Match {
 	 * @return string URL
 	 */
 	public function get_url() {
-		// Remove query params
+		// Remove query params, and decode any encoded characters
 		$url = new Red_Url_Path( $this->url );
 		$path = $url->get_without_trailing_slash();
+
+		// URL encode
+		$decode = [
+			'/',
+			':',
+			'[',
+			']',
+			'@',
+			'~',
+			',',
+			'(',
+			')',
+			';',
+		];
+
+		// URL encode everything - this converts any i10n to the proper encoding
+		$path = rawurlencode( $path );
+
+		// We also converted things we dont want encoding, such as a /. Change these back
+		foreach ( $decode as $char ) {
+			$path = str_replace( rawurlencode( $char ), $char, $path );
+		}
 
 		// Lowercase everything
 		$path = mb_strtolower( $path );
