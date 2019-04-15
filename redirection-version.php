5a5cc0935daecc07df13f8ed39afc690ef31bcce
John Godley <johng75@gmail.com>
2019-04-15 18:28:26 +0100

    Release 4.2.3

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.2.2' );
-define( 'REDIRECTION_BUILD', 'e0416ca80560a4011025c96bafa6c3a0' );
+define( 'REDIRECTION_VERSION', '4.2.3' );
+define( 'REDIRECTION_BUILD', '832f7155834bcdc64325009360c6273b' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
