42b811f86567a044f334fc0aa53c5518073ad70d
John Godley <johng75@gmail.com>
2019-04-27 16:29:09 +0100

    Package 4.3 beta 1

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.3-alpha-1' );
-define( 'REDIRECTION_BUILD', 'd6343ecfdc0320b48e285f156e430205' );
+define( 'REDIRECTION_VERSION', '4.3-beta-1' );
+define( 'REDIRECTION_BUILD', '749c88d89b5da76c6effe554a9780110' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
