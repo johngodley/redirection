c53f3323eee3ed27a1defe37eb38c8386e88f047
John Godley <johng75@gmail.com>
2019-05-12 07:49:52 +0100

    Paginate through multisite DB action
    Multi site can have > 100, so paginate through the sites

--- database/database.php
+++ database/database.php
@@ -77,13 +77,19 @@ class Red_Database {
 
 	public static function apply_to_sites( $callback ) {
 		if ( is_multisite() && ( is_network_admin() || defined( 'WP_CLI' ) && WP_CLI ) ) {
-			array_map( function( $site ) use ( $callback ) {
-				switch_to_blog( $site->blog_id );
+			$total = get_sites( [ 'count' => true ] );
+			$per_page = 100;
 
-				$callback();
+			// Paginate through all sites and apply the callback
+			for ( $offset = 0; $offset < $total; $offset += $per_page ) {
+				array_map( function( $site ) use ( $callback ) {
+					switch_to_blog( $site->blog_id );
 
-				restore_current_blog();
-			}, get_sites() );
+					$callback();
+
+					restore_current_blog();
+				}, get_sites( [ 'number' => $per_page, 'offset' => $offset ] ) );
+			}
 
 			return;
 		}
