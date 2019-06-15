0b0cc942553c6ef9f0a737dc0c8d9469f88eb387
John Godley <johng75@gmail.com>
2019-06-15 15:40:24 +0100

    Build 4.3.2 beta 1

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://redirection.me/donation/
 Tags: redirect, htaccess, 301, 404, seo, permalink, apache, nginx, post, admin
 Requires at least: 4.8
 Tested up to: 5.2.1
-Stable tag: 4.3.1
+Stable tag: 4.3.2
 Requires PHP: 5.4
 License: GPLv3
 
--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.3.1' );
-define( 'REDIRECTION_BUILD', '76c2e333a0fb0e417d3c1937d0c4045c' );
+define( 'REDIRECTION_VERSION', '4.3.2-beta-1' );
+define( 'REDIRECTION_BUILD', '7b1ef1a190deaea2c8a3367efce6b384' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
