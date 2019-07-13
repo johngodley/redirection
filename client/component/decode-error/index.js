e722edbe9d9fa65cb6f93d2ddec49c3b0cc75f42
John Godley <johng75@gmail.com>
2019-07-13 14:26:04 +0100

    Don’t report a plugin error if we return a valid error code

--- client/component/decode-error/index.js
+++ client/component/decode-error/index.js
@@ -11,7 +11,7 @@ import { translate as __ } from 'lib/locale';
 
 import ExternalLink from 'component/external-link';
 
-const isSecurityPlugin = ( status, code ) => [ 400, 401, 403, 405 ].indexOf( status ) !== -1 || code === 'rest_no_route';
+const isSecurityPlugin = ( status, code ) => ( [ 400, 401, 403, 405 ].indexOf( status ) !== -1 || code === 'rest_no_route' ) && parseInt( code, 10 ) === 0;
 const isServerError = status => [ 500, 502, 503 ].indexOf( status ) !== -1;
 const extractPhpError = ( { raw } ) => {
 	const parts = raw.split( '<br />' ).filter( item => item );
