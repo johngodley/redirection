afbb4e3e67ade95f02b2e560270f4aa5a37488a4
John Godley <johng75@gmail.com>
2019-07-21 10:34:03 +0100

    Fix problem decoding a regex with a + in it

--- models/regex.php
+++ models/regex.php
@@ -8,7 +8,7 @@ class Red_Regex {
 	private $case;
 
 	public function __construct( $pattern, $case_insensitive = false ) {
-		$this->pattern = urldecode( $pattern );
+		$this->pattern = rawurldecode( $pattern );
 		$this->case = $case_insensitive;
 	}
 
