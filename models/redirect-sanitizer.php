8b4cff34e2d498916bab65ea5e6563d3acac206f
John Godley <johng75@gmail.com>
2019-08-21 06:31:51 +0100

    Add code to validate HTTP codes

--- models/redirect-sanitizer.php
+++ models/redirect-sanitizer.php
@@ -148,9 +148,17 @@ class Red_Item_Sanitize {
 		return false;
 	}
 
+	public function is_valid_redirect_code( $code ) {
+		return in_array( $code, array( 301, 302, 303, 304, 307, 308 ), true );
+	}
+
+	public function is_valid_error_code( $code ) {
+		return in_array( $code, array( 400, 401, 403, 404, 410, 418 ), true );
+	}
+
 	protected function get_code( $action_type, $code ) {
 		if ( $action_type === 'url' || $action_type === 'random' ) {
-			if ( in_array( $code, array( 301, 302, 303, 304, 307, 308 ), true ) ) {
+			if ( $this->is_valid_redirect_code( $code ) ) {
 				return $code;
 			}
 
@@ -158,7 +166,7 @@ class Red_Item_Sanitize {
 		}
 
 		if ( $action_type === 'error' ) {
-			if ( in_array( $code, array( 400, 401, 403, 404, 410, 418 ), true ) ) {
+			if ( $this->is_valid_error_code( $code ) ) {
 				return $code;
 			}
 
