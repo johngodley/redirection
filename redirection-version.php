73b50fe01268070140387bbec7881a2c7215d027
John Godley <johng75@gmail.com>
2019-09-07 15:22:44 +0100

    Rebuild 4.4 beta 2

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '4.4-beta-2' );
-define( 'REDIRECTION_BUILD', 'cd81aec04d036d458699bfbf2867fd67' );
+define( 'REDIRECTION_BUILD', '1d67c690298d23a502e791da273bc0a3' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
