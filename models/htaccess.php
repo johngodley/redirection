725be4ebe2c61c8862ed7a0d717505b119f469b9
John Godley <johng75@gmail.com>
2019-10-13 12:27:51 +0100

    Ampersand without ? does not denote query string
    Props to @Sophist-UK in https://github.com/johngodley/redirection/pull/169

--- models/htaccess.php
+++ models/htaccess.php
@@ -129,7 +129,7 @@ class Red_Htaccess {
 	private function add_url( $item, $match ) {
 		$url = $item->get_url();
 
-		if ( $item->is_regex() === false && strpos( $url, '?' ) !== false || strpos( $url, '&' ) !== false ) {
+		if ( $item->is_regex() === false && strpos( $url, '?' ) !== false ) {
 			$url_parts = wp_parse_url( $url );
 			$url = $url_parts['path'];
 			$query = isset( $url_parts['query'] ) ? $url_parts['query'] : '';
