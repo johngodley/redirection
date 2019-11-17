046f562cc34a8bb10a6150ac4d6b564f2211ac87
John Godley <johng75@gmail.com>
2019-11-17 09:14:57 +0000

    Bundle 4.5

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.4.2' );
-define( 'REDIRECTION_BUILD', 'dd4f7bc71ce67ad6763df5e0b4a4886a' );
+define( 'REDIRECTION_VERSION', '4.5' );
+define( 'REDIRECTION_BUILD', 'c6e5493bcd261ad0bf901f7bb91123a6' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
