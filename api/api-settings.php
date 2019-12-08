a4902315b12d46a118ffdf110f03486ee5e74e2b
John Godley <johng75@gmail.com>
2019-12-08 15:48:37 +0000

    Update API docs with new settings

--- api/api-settings.php
+++ api/api-settings.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @api {get} /redirection/v1/setting Get settings
  * @apiName GetSettings
@@ -51,6 +50,9 @@
  * @apiSuccess {String} settings.https
  * @apiSuccess {String} settings.headers
  * @apiSuccess {String} settings.database
+ * @apiSuccess {String} settings.relcoate Relocate this site to the specified domain (and path)
+ * @apiSuccess {String="www","nowww",""} settings.preferred_domain Preferred canonical domain
+ * @apiSuccess {String[]} settings.aliases Array of domains that will be redirected to the current WordPress site
  * @apiSuccess {Object[]} groups An array of groups
  * @apiSuccess {String} groups.label Name of the group
  * @apiSuccess {Integer} groups.value Group ID
