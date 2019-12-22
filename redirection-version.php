b438078519562df83f641a9afd16d5d5e73fc41d
John Godley <johng75@gmail.com>
2019-12-22 10:33:00 +0000

    Rebundle 4.6 with style fixes
    style-loader 1.1.1 breaks the page styles

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '4.6' );
-define( 'REDIRECTION_BUILD', 'ab66c4ec560a0522cf44f4f13a5837fe' );
+define( 'REDIRECTION_BUILD', '1a054fbd427b8470b204f1a2dbd12b4a' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
