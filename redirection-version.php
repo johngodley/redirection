f610fff10bc99ba013a3237e64df0ff6bf5b8684
John Godley <johng75@gmail.com>
2022-01-15 07:31:07 +0000

    Update version file for 5.2

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '5.2' );
-define( 'REDIRECTION_BUILD', '7417dadd6fe9ddce2a1c8eae3ead9c4f' );
+define( 'REDIRECTION_BUILD', '8bf709f8ad2a8ee577aaec2e9e993c42' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
