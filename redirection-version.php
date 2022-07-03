b5ee46cb8e6ea5f4edec7096753b6d07db1ebca0
John Godley <johng75@gmail.com>
2022-07-03 12:32:21 +0100

    5.3 beta 1

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.2.3' );
-define( 'REDIRECTION_BUILD', 'cb109e7c800603ff2d5cc098451320be' );
-define( 'REDIRECTION_MIN_WP', '4.6' );
+define( 'REDIRECTION_VERSION', '5.3-beta-1' );
+define( 'REDIRECTION_BUILD', '31c93582945c6844c769215715cac577' );
+define( 'REDIRECTION_MIN_WP', '5.4' );
