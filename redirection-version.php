0c0f69dbefbc8da18437c375a6f28f785e2dc507
John <johng75@gmail.com>
2022-09-14 12:10:26 +0100

    Fixes for 5.3.4 (#3412)
    * Fix query params where a + is in the name
    
    Need to fix both the match and the parameter passing
    
    * Bundle 5.3.4
    
    * Update locale files

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.3.3' );
-define( 'REDIRECTION_BUILD', 'df596e91f760a31732345ea55e190a72' );
+define( 'REDIRECTION_VERSION', '5.3.4' );
+define( 'REDIRECTION_BUILD', '04d536d277e6e20820f24f21d395db08' );
 define( 'REDIRECTION_MIN_WP', '5.4' );
