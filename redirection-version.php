228538aa6a987c50f8a8e70194c0030f202affab
John <johng75@gmail.com>
2022-11-06 11:08:33 +0000

    Update version 5.3.5 (#3465)
    * Update locales
    
    * Bundle 5.3.5

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.3.4' );
-define( 'REDIRECTION_BUILD', '04d536d277e6e20820f24f21d395db08' );
+define( 'REDIRECTION_VERSION', '5.3.5' );
+define( 'REDIRECTION_BUILD', 'f0dac5b2b3267cd737c1a679e4c1fac3' );
 define( 'REDIRECTION_MIN_WP', '5.4' );
