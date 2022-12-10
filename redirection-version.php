13e47b27c62df6b5635ceb9b29ee38ee45164af9
John <johng75@gmail.com>
2022-12-10 06:37:44 +0000

    Fix CZ again (#3500)

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '5.3.6' );
-define( 'REDIRECTION_BUILD', '852dc40b78f863aa321e5c393a7a4259' );
+define( 'REDIRECTION_BUILD', 'ff1aa31d162d8fc62424842d9ba54ee6' );
 define( 'REDIRECTION_MIN_WP', '5.4' );
