0d741e785f72cba06ee462d829fbaf9211044783
John Godley <johng75@gmail.com>
2022-12-10 06:41:51 +0000

    Update build files

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'REDIRECTION_VERSION', '5.3.6' );
-define( 'REDIRECTION_BUILD', 'ff1aa31d162d8fc62424842d9ba54ee6' );
+define( 'REDIRECTION_BUILD', '16d9cc72e7ac56bb977b78fce03e6ba0' );
 define( 'REDIRECTION_MIN_WP', '5.4' );
