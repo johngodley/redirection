75642322d16f25790f6b4bf9ddd644742b05de22
John Godley <johng75@gmail.com>
2024-01-05 09:06:27 +0000

    Bundle

--- redirection-version.php
+++ redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.4' );
-define( 'REDIRECTION_BUILD', 'b606c89b7105268adfc5842c492f91a0' );
+define( 'REDIRECTION_VERSION', '5.4.1' );
+define( 'REDIRECTION_BUILD', '4921947a93e8d2bdecfa002a29093c22' );
 define( 'REDIRECTION_MIN_WP', '5.7' );
