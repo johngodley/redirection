2dae7f6273398e7cfe06d362611f9a3fda1bc36c
Pierre PÉRONNET <pierre.peronnet@datadoghq.com>
2024-02-09 19:07:46 +0100

    hook(redirection_export_groups) Introduce filter to enrich groups list before export
    Signed-off-by: Pierre PÉRONNET <pierre.peronnet@datadoghq.com>

--- models/file-io.php
+++ models/file-io.php
@@ -77,7 +77,8 @@ abstract class Red_FileIO {
 			$groups = Red_Group::get_all();
 			$items = Red_Item::get_all();
 
-			$items = self::filter_items_to_export( 'all', $items );
+			$groups = self::filter_groups_to_export( 'all', $groups );
+			$items = self::filter_items_to_export( 'all', $items, $groups );
 		} else {
 			$module_name_or_id = is_numeric( $module_name_or_id ) ? $module_name_or_id : Red_Module::get_id_for_name( $module_name_or_id );
 			$module = Red_Module::get( intval( $module_name_or_id, 10 ) );
@@ -86,7 +87,8 @@ abstract class Red_FileIO {
 				$groups = Red_Group::get_all_for_module( $module->get_id() );
 				$items = Red_Item::get_all_for_module( $module->get_id() );
 
-				$items = self::filter_items_to_export( $module->get_name(), $items );
+				$groups = self::filter_groups_to_export( $module->get_name(), $groups );
+				$items = self::filter_items_to_export( $module->get_name(), $items, $groups );
 			}
 		}
 
@@ -102,11 +104,18 @@ abstract class Red_FileIO {
 		return false;
 	}
 
-	private static function filter_items_to_export( $module_name, $items ) {
-		$items = apply_filters( 'redirection_export_items', $items );
-		$items = apply_filters( 'redirection_export_items_' . $module_name, $items );
+	private static function filter_items_to_export( $module_name, $items, $groups ) {
+		$items = apply_filters( 'redirection_export_items', $items, $groups );
+		$items = apply_filters( 'redirection_export_items_' . $module_name, $items, $groups );
 
 		return $items;
+	}
+
+	private static function filter_groups_to_export( $module_name, $groups ) {
+		$groups = apply_filters( 'redirection_export_groups', $groups );
+		$groups = apply_filters( 'redirection_export_groups_' . $module_name, $groups );
+
+		return $groups;
 	}
 
 	abstract public function get_data( array $items, array $groups );
