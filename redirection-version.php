c8082914422adf821621f9d3dd15e91edf056630
John Godley <johng75@gmail.com>
2024-08-10 11:33:03 +0100

    Add release-it

--- redirection-version.php
+++ redirection-version.php
@@ -2,4 +2,4 @@
 
 define( 'REDIRECTION_VERSION', '5.5.0' );
 define( 'REDIRECTION_MIN_WP', '6.4' );
-define( 'REDIRECTION_BUILD', 'fe0a39d2435bdc89725a505d988b3ee2' );
+define( 'REDIRECTION_BUILD', 'eff218e8a6050e14b616f8d6629e0ae4' );
