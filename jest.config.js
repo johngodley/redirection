e2f640a1f8f0c8ce9de4288e929082561169aeb2
John Godley <john@johngodley.com>
2026-01-17 13:21:38 +0000

    Update Jest to fix tests

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,11 @@ module.exports = {
 		...defaultConfig.moduleNameMapper,
 		'^@wp-plugin-lib$': '<rootDir>/src/wp-plugin-lib',
 		'^@wp-plugin-components$': '<rootDir>/src/wp-plugin-components',
+		'^lib/(.*)$': '<rootDir>/src/lib/$1',
+		'^component/(.*)$': '<rootDir>/src/component/$1',
+		'^state/(.*)$': '<rootDir>/src/state/$1',
+		'^page/(.*)$': '<rootDir>/src/page/$1',
+		'^stores/(.*)$': '<rootDir>/src/stores/$1',
+		'^types/(.*)$': '<rootDir>/src/types/$1',
 	},
 };
