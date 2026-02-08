1a8deebd2f46617f7dfdac1cf7907382497e2a5f
saurab1996 <saurabgupta@goldenmace.com>
2026-02-08 20:28:21 +0530

    remove client folder since it's for local development

--- client/wp-plugin-components/error/types.js
+++
@@ -1,9 +0,0 @@
-/**
- * @typedef ApiError
- * @type
- * @property {number} code
- * @property {object} data
- * @property {string} data.wpdb
- * @property {string} message
- * @property {object} request
- */
--- client/wp-plugin-lib/index.js
+++
@@ -1,5 +0,0 @@
-export { default as apiFetch } from './api-fetch';
-export { default as PageRouter } from './page-router';
-export { default as Portal } from './portal';
-export { deleteApiRequest, uploadApiRequest, postApiRequest, getApiRequest } from './api-fetch/api-method';
-export { setPageUrl, getPluginPage, getWordPressUrl, getPageUrl, removeFromPageUrl } from './wordpress-url';
