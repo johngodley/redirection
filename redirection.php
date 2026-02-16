a9317fc62f0ed8c480ec0fd710fdded62a68bcf1
John <john@johngodley.com>
2026-02-16 08:03:12 +0000

    Ready 5.7.1 (#4140)
    * Ready 5.7.1
    
    * Ensure min WP matches readme

--- redirection.php
+++ redirection.php
@@ -3,7 +3,7 @@
 Plugin Name: Redirection
 Plugin URI: https://redirection.me/
 Description: Manage all your 301 redirects and monitor 404 errors
-Version: 5.7
+Version: 5.7.1
 Author: John Godley
 Text Domain: redirection
 Requires PHP: 7.4
@@ -41,9 +41,9 @@ if ( version_compare( PHP_VERSION, '7.4' ) < 0 ) {
 if ( file_exists( __DIR__ . '/build/redirection-version.php' ) ) {
 	require_once __DIR__ . '/build/redirection-version.php';
 } else {
-	define( 'REDIRECTION_VERSION', '5.7.0' );
+	define( 'REDIRECTION_VERSION', '5.7.1' );
 	define( 'REDIRECTION_BUILD', '951ae5be919314a6585a78d8091577b5' );
-	define( 'REDIRECTION_MIN_WP', '6.4' );
+	define( 'REDIRECTION_MIN_WP', '6.5' );
 }
 
 require_once __DIR__ . '/redirection-settings.php';
