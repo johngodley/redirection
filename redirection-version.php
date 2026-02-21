b347bfc6c069048809c4de0ab3c94b087a6628ce
John Godley <john@johngodley.com>
2026-02-21 07:40:53 +0000

    Bump versions for 5.7.3

--- redirection-version.php
+++ redirection-version.php
@@ -1,6 +1,6 @@
 <?php
 
 // This is a dummy file to help with cached sites
-define( 'REDIRECTION_VERSION', '5.7.2' );
-define( 'REDIRECTION_BUILD', '2612cf68d9981df94efc7d166bed264a' );
+define( 'REDIRECTION_VERSION', '5.7.3' );
+define( 'REDIRECTION_BUILD', '4226b5957b97db33450c269681d4a29e' );
 define( 'REDIRECTION_MIN_WP', '6.5' );
