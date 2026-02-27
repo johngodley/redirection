a3dbb9217396354c543507278e7b4e71c1fe65ab
John Godley <john@johngodley.com>
2026-02-27 17:50:59 +0000

    Add strikethrough to redirectflag
    If the flag is different from the default, and should show it being disabled then show a strikethrough style so its clear

--- src/page/redirects/columns/source-flag.tsx
+++ src/page/redirects/columns/source-flag.tsx
@@ -4,10 +4,15 @@ import Badge from '@wp-plugin-components/badge';
 interface RedirectFlagProps {
 	name: string;
 	className?: string;
+	strikethrough?: boolean;
 }
 
-function RedirectFlag( { name, className }: RedirectFlagProps ) {
-	return <Badge className={ clsx( 'redirect-source__flag', className ) }>{ name }</Badge>;
+function RedirectFlag( { name, className, strikethrough }: RedirectFlagProps ) {
+	return (
+		<Badge className={ clsx( 'redirect-source__flag', className, { 'redirect-source__flag--disabled': strikethrough } ) }>
+			{ strikethrough ? <s>{ name }</s> : name }
+		</Badge>
+	);
 }
 
 export default RedirectFlag;
