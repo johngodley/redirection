75dfded43d052f534d9a1530bef8da754c79fa85
John Godley <john@johngodley.com>
2026-07-04 13:59:20 +0100

    Fix CI

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ define( 'REDIRECTION_MIN_WP', '${ pkg.wordpress.supported }' );
 
 function generateVersion( compilation ) {
 	const versionFile = path.resolve( __dirname, 'build/redirection-version.php' );
+	const versionDir = path.dirname( versionFile );
 	const asset = compilation.getAsset( 'redirection.js' );
 
 	if ( ! asset ) {
@@ -31,6 +32,7 @@ function generateVersion( compilation ) {
 	const data = asset.source.source();
 	const md5 = crypto.createHash( 'md5' ).update( data ).digest( 'hex' );
 
+	fs.mkdirSync( versionDir, { recursive: true } );
 	fs.writeFileSync( versionFile, versionHeader( md5 ) );
 }
 
