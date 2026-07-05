a152ed52581c615b2c3fbd964e482f8b8dcb773d
John Godley <john@johngodley.com>
2026-07-05 08:30:13 +0100

    Nginx error handling

--- tests/integration/import-export/test-nginx.php
+++ tests/integration/import-export/test-nginx.php
@@ -68,4 +68,41 @@ class NginxTest extends WP_UnitTestCase {
 
 		$this->assertEquals( 'rewrite (?i)^/test$ /target permanent;', trim( $lines[5] ) );
 	}
+
+	public function testErrorCodeUsesExactLocation() {
+		$nginx = new Nginx();
+		$redirects = [ new Red_Item( (object) [ 'match_type' => 'url', 'id' => 1, 'action_type' => 'error', 'url' => '/gone', 'action_code' => 410 ] ) ];
+
+		$file = $nginx->get_data( $redirects, [] );
+		$lines = explode( "\n", $file );
+
+		$this->assertEquals( 'location = /gone {', trim( $lines[5] ) );
+		$this->assertEquals( 'return 410;', trim( $lines[6] ) );
+		$this->assertEquals( '}', trim( $lines[7] ) );
+	}
+
+	public function testErrorCodeUsesRegexLocationWhenCaseInsensitive() {
+		$match_data = json_encode( [ 'source' => [ 'flag_case' => true ] ] );
+		$nginx = new Nginx();
+		$redirects = [ new Red_Item( (object) [ 'match_type' => 'url', 'id' => 1, 'action_type' => 'error', 'url' => '/test', 'action_code' => 451, 'match_data' => $match_data ] ) ];
+
+		$file = $nginx->get_data( $redirects, [] );
+		$lines = explode( "\n", $file );
+
+		$this->assertEquals( 'location ~* ^/test$ {', trim( $lines[5] ) );
+		$this->assertEquals( 'return 451;', trim( $lines[6] ) );
+		$this->assertEquals( '}', trim( $lines[7] ) );
+	}
+
+	public function testErrorCodeUsesRegexLocationWhenRegex() {
+		$nginx = new Nginx();
+		$redirects = [ new Red_Item( (object) [ 'match_type' => 'url', 'id' => 1, 'regex' => true, 'action_type' => 'error', 'url' => '^/test.*$', 'action_code' => 410 ] ) ];
+
+		$file = $nginx->get_data( $redirects, [] );
+		$lines = explode( "\n", $file );
+
+		$this->assertEquals( 'location ~ ^/test.*$ {', trim( $lines[5] ) );
+		$this->assertEquals( 'return 410;', trim( $lines[6] ) );
+		$this->assertEquals( '}', trim( $lines[7] ) );
+	}
 }
