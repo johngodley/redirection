ead566645f27d3990223b60cc942088229d5a3b1
John Godley <john@johngodley.com>
2026-08-02 10:41:44 +0100

    Apply fixes

--- tests/bootstrap-unit.php
+++ tests/bootstrap-unit.php
@@ -4,9 +4,9 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 define( 'PLUGIN_PATH', dirname( __DIR__ ) );
 
-$redirection_refactor = getenv( 'REDIRECTION_REFACTOR' );
+$redirection_refactor = filter_var( getenv( 'REDIRECTION_REFACTOR' ), FILTER_VALIDATE_BOOLEAN );
 
-if ( $redirection_refactor !== false && $redirection_refactor !== '' && ! defined( 'REDIRECTION_REFACTOR' ) ) {
+if ( $redirection_refactor && ! defined( 'REDIRECTION_REFACTOR' ) ) {
 	define( 'REDIRECTION_REFACTOR', true );
 }
 
