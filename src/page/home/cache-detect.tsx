40c17f52c26034430f1ff6b489c77c424aa63b94
John Godley <john@johngodley.com>
2026-08-29 14:42:39 +0100

    Improve cache detection message

--- src/page/home/cache-detect.tsx
+++ src/page/home/cache-detect.tsx
@@ -1,6 +1,10 @@
 import { __ } from '@wordpress/i18n';
-import { ExternalLink, Error } from '@wp-plugin-components';
-import { getErrorLinks, getErrorDetails, getCacheBuster } from 'lib/error-links';
+import { ExternalLink, Error, Button, createInterpolateElement } from '@wp-plugin-components';
+import { getErrorLinks, getErrorDetails, getCacheBuster, getReloadUrl } from 'lib/error-links';
+
+function reloadWithoutCache() {
+	window.location.href = getReloadUrl();
+}
 
 function CacheDetect() {
 	return (
@@ -12,12 +16,27 @@ function CacheDetect() {
 			links={ getErrorLinks() }
 			locale="redirection"
 		>
-			<p>{ __( 'Please clear your browser cache and reload this page.', 'redirection' ) }</p>
 			<p>
-				{ __( 'If you are using a caching system such as Cloudflare then please read this:', 'redirection' ) }
-				<ExternalLink url="https://redirection.me/support/problems/cloudflare/">
-					{ __( 'clearing your cache.', 'redirection' ) }
-				</ExternalLink>
+				{ __(
+					'This page was loaded from an old cached copy of the WordPress admin area. This is usually caused by a caching system, such as Cloudflare, LiteSpeed, or your host, rather than by your own browser.',
+					'redirection'
+				) }
+			</p>
+			<p>
+				<Button isPrimary onClick={ reloadWithoutCache }>
+					{ __( 'Reload without cache', 'redirection' ) }
+				</Button>
+			</p>
+			<p>
+				{ createInterpolateElement(
+					__(
+						'If reloading does not fix this then ask your host or caching provider to exclude the WordPress admin area (/wp-admin/) from any page caching. If you are using Cloudflare then please read this: {{link}}clearing your cache.{{/link}}',
+						'redirection'
+					),
+					{
+						link: <ExternalLink url="https://redirection.me/support/problems/cloudflare/" />,
+					}
+				) }
 			</p>
 		</Error>
 	);
